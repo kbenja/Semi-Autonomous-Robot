5f350bdb9c6d6a7892fec41a799c9bd828d32443
Daniel Gorelick <dqgorelick@gmail.com>
2015-11-10 18:30:05 -0500

    [DG] WIP interactive CLI

--- LIDAR_Edge_Detection/LIDAR_Edge_Detection.ino
+++ LIDAR_Edge_Detection/LIDAR_Edge_Detection.ino
@@ -1,5 +1,5 @@
 //**EDGE DETECTION PROGRAM**//
-//By Pete Benjathatchaporn 
+//By Pete Benjathatchaporn, Daniel Gorelick
 //Created Nov 9th, 2015
 
 int analogInput = A0;//Input from the range sensor
@@ -7,13 +7,48 @@ float currentDistance; //Current Reading of the range Sensor
 float lastDistance; //Last Reading of the range Sensor
 float delta = 0;
 
-//This can be change depending on the size of the Breaker
+int state = 0; //Variable for state machine
+bool printed = false; //Boolean for printing statement
+
 float deltaDesired = 0.10; //Currently set at 0.15 meters - Less = More Sensitive
+float distanceDesired = 0.30; 
+float distanceError = 0.05; //Error of margin for distance
 
 void setup() 
 {
   pinMode(analogInput, INPUT); // Initialize A0 as INPUT
   Serial.begin(9600);
+}
+
+bool detectEdge()
+{
+  //Calculate the difference in Distance (delta)
+  if (currentDistance < lastDistance)
+  {
+    //This ensures that delta is always Positive
+    delta = lastDistance - currentDistance;
+  }
+  else 
+  {
+    delta = currentDistance - lastDistance;
+  }
+  
+  //If delta is greater than X meters, EDGE is detected
+  if (delta != currentDistance)
+  {
+    if(delta >= deltaDesired)
+    {
+      return true;
+    }
+  }  
+}
+
+bool detectDistance(float desired)
+{
+  if(currentDistance + distanceError/2 > desired && currentDistance - distanceError/2 < desired)
+  {
+    return true;
+  }
 }
 
 void loop() 
@@ -24,30 +59,43 @@ void loop()
   //Convert Analog Reading to Distance in meters
   currentDistance = analogReading * ((5.0)/(1023));
 
-  //Serial.println(currentDistance);
-
-  //Calculate the difference in Distance (delta)
-  if (currentDistance < lastDistance)
-  {
-    //This ensures that delta is always Positive
-    delta = lastDistance - currentDistance;
-  }
-  else 
-  {
-    delta = currentDistance -lastDistance;
-  }
+  /*  State machine
+   *  1: move forward within certain range
+   *  2: pass first edge
+   *  3: pass second edge
+   *  4: go back into the middle
+   *  5: line up between the edges 
+   *  6: move forward to certain range
+   */
+   
+  switch(state){
+    case 0:
+      if (!printed){
+        Serial.println("Find first edge");
+        printed = true;
+      }
+      if (detectEdge()) {
+        Serial.println("EDGE DETECTED");
+        state = 1;
+        printed = false;
+      } 
+    case 1:
+      if(!printed){
+        Serial.println("Move to 0.30 meters");
+        printed = true;
+      }    
+      if (detectDistance){
+        Serial.println("CORRECT DISTANCE");
+        state = 0;
+        printed = false;
+      } else {
+        Serial.print("CURRENT DISTANCE = ");
+        Serial.print(currentDistance);
+        Serial.print("\r");
+      }
+  } 
   
-  //If delta is greater than X meters, EDGE is detected
-  if (delta != currentDistance)
-  {
-    if(delta >= deltaDesired)
-    {
-      Serial.println("!!!!!EDGE DETECTED!!!!!");
-    }
-  }
-
-  delay(100); //Delay dictates time limit before lastDistance gets overwrite
-
   lastDistance = currentDistance;
 
+  delay(100); //Delay dictates time limit before lastDistance gets overwrite
 }
