7d5a5cb12375fbcdf7f87ce64d98c07b85467057
Daniel Gorelick <dqgorelick@gmail.com>
2016-03-21 11:48:32 -0400

    changes to double_reg library and navx class

--- breakerbot/include/navx_module.h
+++ breakerbot/include/navx_module.h
@@ -5,7 +5,28 @@
 
 class NavX_Module {
 public:
+    NavX_Module() {};
+    float get_yaw() {
+        mraa_i2c_context i2c = mraa_i2c_init(6);
+        mraa_result_t result = MRAA_SUCCESS;
+        result = mraa_i2c_address(i2c, 0x32);
+        printf((result != MRAA_SUCCESS) ? "was not able to connect to address\n", "Was able to write to the device\n");
+        uint8_t high_bits;
+        uint8_t low_bits;
+        double_reg combined;
 
+        result = mraa_i2c_write_byte(i2c, 0x16);
+        result = mraa_i2c_read(i2c, &high_bits, 1);
 
+        result = mraa_i2c_write_byte(i2c, 0x17);
+        result = mraa_i2c_read(i2c, &low_bits, 1);
+
+        combined.upper = high_bits;
+        combined.lower = low_bits;
+
+        printf("Yaw: High: %d, Low: %d\n", high_bits, low_bits);
+        printf("Combined value = 0x%04x, decimal = %f", combined.u_sixteen, (float)combined.u_sixteen/100.0);
+        return (float)combined.u_sixteen/100.0;
+    }
 };
 
---
+++ breakerbot/src/double_reg_test.cpp
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+union double_reg {
+    uint8_t     u_eight[2];
+    uint16_t    u_sixteen;
+    struct      {uint8_t upper, lower;};
+};
+
+int main() {
+    double_reg test;
+    // test.lower = 0x12;
+    // test.upper = 0x34;
+    // test.u_eight[1] = 0x12;
+    // test.u_eight[0] = 0x34;
+    test.u_sixteen = 0x1234;
+    printf("LOWER = 0x%02x, UPPER = 0x%02x\n", test.lower, test.upper);
+    printf("u_eight[1] = 0x%02x, u_eight[0] = 0x%02x\n", test.u_eight[1], test.u_eight[0]);
+    printf("test.u_sixteen = 0x%04x decimal = %d\n", test.u_sixteen, test.u_sixteen);
+    return 0;
+}
