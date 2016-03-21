c351c9e54e638f114b8060a080acff7e69dcc5ca
Daniel Gorelick <dqgorelick@gmail.com>
2016-03-21 17:25:29 +0000

    receiving output from NavX, need to convert from using to signed int

--- breakerbot/include/navx_module.h
+++ breakerbot/include/navx_module.h
@@ -5,21 +5,28 @@
 
 class NavX_Module {
 public:
-    NavX_Module() {};
+    NavX_Module() {}
     float get_yaw() {
         mraa_i2c_context i2c = mraa_i2c_init(6);
         mraa_result_t result = MRAA_SUCCESS;
         result = mraa_i2c_address(i2c, 0x32);
-        printf((result != MRAA_SUCCESS) ? "was not able to connect to address\n", "Was able to write to the device\n");
+        if(result != MRAA_SUCCESS){
+            printf("was not able to connect to address\n");
+        } else {
+            printf("Was able to write to the device\n");
+        }
         uint8_t high_bits;
         uint8_t low_bits;
         double_reg combined;
+        int status = 1;
+        status = mraa_i2c_write_byte(i2c, 0x16);
+        status = mraa_i2c_read(i2c, &high_bits, 1);
 
-        result = mraa_i2c_write_byte(i2c, 0x16);
-        result = mraa_i2c_read(i2c, &high_bits, 1);
-
-        result = mraa_i2c_write_byte(i2c, 0x17);
-        result = mraa_i2c_read(i2c, &low_bits, 1);
+        status = mraa_i2c_write_byte(i2c, 0x17);
+        status = mraa_i2c_read(i2c, &low_bits, 1);
+        if(status != 1) {
+            printf("Could not read / write from the navx board\n");
+        }
 
         combined.upper = high_bits;
         combined.lower = low_bits;
@@ -30,3 +37,4 @@ public:
     }
 };
 
+#endif
