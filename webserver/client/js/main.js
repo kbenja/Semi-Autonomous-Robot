34eda84c2fe5b9095fc38a4f21f7cf7370986b8d
Daniel Gorelick <dqgorelick@gmail.com>
2016-04-03 13:10:22 -0400

    adding tracking library and test example

--- webserver/client/js/main.js
+++ webserver/client/js/main.js
@@ -1,38 +1,54 @@
 var display_manual = true;
 
 function change_mode() {
-    if(display_manual) {
-        $(".manual").css("display","none");
-        $(".auto").css("display","block");
+    if (display_manual) {
+        $(".manual").css("display", "none");
+        $(".auto").css("display", "block");
     } else {
-        $(".manual").css("display","block");
-        $(".auto").css("display","none");
+        $(".manual").css("display", "block");
+        $(".auto").css("display", "none");
     }
     display_manual = !display_manual;
 }
 
 
-$(document).ready(function(){
+$(document).ready(function() {
     // init canvas and display loading text
     var canvas = document.getElementById('canvas-video');
     var ctx = canvas.getContext('2d');
     ctx.fillStyle = '#333';
-    ctx.fillText('Loading...', canvas.width/2-30, canvas.height/3);
+    ctx.fillText('Loading...', canvas.width / 2 - 30, canvas.height / 3);
 
-    getFile("../ip.txt", function(response){
+    getFile("../ip.txt", function(response) {
         // initialize websocket
         var wsUrl = response;
         var client = new WebSocket(wsUrl);
-        var player = new jsmpeg(client, { canvas:canvas });
+        var player = new jsmpeg(client, { canvas: canvas });
 
         // initialize controller
         var controller = new Controller(client);
         controller.initialize();
+
         function cycle(elapsed) {
             controller.readGamePad();
         }
 
         // initialize 60fps loop
         loopManager.run(cycle)
+
+        // tracking colors:
+        var colors = new tracking.ColorTracker(['magenta', 'cyan', 'yellow']);
+
+        colors.on('track', function(event) {
+            if (event.data.length === 0) {
+                console.log("did not find any colors!");
+            } else {
+                event.data.forEach(function(rect) {
+                    console.log(rect.x, rect.y, rect.height, rect.width, rect.color);
+                });
+            }
+        });
+
+        tracking.track('#canvas-video', colors);
     });
 })
