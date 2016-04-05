fdc76e4a8532109a61c48e7b189b534dfffcb6c1
Daniel Gorelick <dqgorelick@gmail.com>
2016-04-05 06:24:20 +0000

    angular integrated!

--- webserver/client/js/app.js
+++ webserver/client/js/app.js
@@ -3,3 +3,13 @@ var app = angular.module ('Breakerbot',
      'Data',
      'CommunicationService'
     ]);
+
+app.controller('DevCtrl', function($scope, $rootScope){
+    $rootScope.manual_mode = true;
+    $scope.status = $rootScope.manual_mode;
+    $scope.toggle_mode = function() {
+        $rootScope.manual_mode = ($rootScope.manual_mode ? false : true);
+        $scope.status = $rootScope.manual_mode;
+        console.log("Toggling mode", $rootScope.manual_mode);
+    }
+});
