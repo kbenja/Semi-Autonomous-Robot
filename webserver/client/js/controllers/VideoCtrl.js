385ba95ee14778953053d107b497ebeb0de6abbb
Daniel Gorelick <dqgorelick@gmail.com>
2016-04-05 22:26:32 +0000

    working detecting two colors

--- webserver/client/js/controllers/VideoCtrl.js
+++ webserver/client/js/controllers/VideoCtrl.js
@@ -1,27 +1,18 @@
 angular.module('Video', ['CommunicationService']).controller('VideoCtrl', function($scope) {
     // computer vision
     $scope.rects = [];
-    var tracker = new tracking.ColorTracker(['yellow']);
+    tracking.ColorTracker.registerColor('red', function(r, g, b) {
+      if (r > 100 && g < 75 && b < 75) {
+        return true;
+      }
+      return false;
+    });
+    var tracker = new tracking.ColorTracker(['yellow','red']);
     tracker.on('track', function(event) {
-        // $scope.rects = document.querySelector('.rect');
-        // if (rects) {
-        //     // $('.rect').remove(); // jquery way
-        //     // javascript way change to this if we use angular
-        //     // Array.prototype.forEach.call(rects, function(node) {
-        //     //     node.parentNode.removeChild(node);
-        //     // });
-        // }
         $scope.rects = event.data;
         $scope.rects.forEach(function(rect) {
 
             console.log(rect);
-            // rect.classList.add('rect');
-            // rect.style["border"] = '2px solid ' + rect.color;
-            // rect.style["position"] = 'absolute';
-            // rect.style["width"] = rect.width + 'px';
-            // rect.style["height"] = rect.height + 'px';
-            // rect.style["left"] = rect.x + 'px';
-            // rect.style["top"] = rect.y + 'px';
         });
     });
     setInterval(function() {
