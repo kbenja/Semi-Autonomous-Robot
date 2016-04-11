0c3d56d9e945d4dca07e895f02c0dfaf72d1112c
Daniel Gorelick <dqgorelick@gmail.com>
2016-04-11 19:45:40 -0400

    updates to the UI

--- webserver/client/js/controllers/DataCtrl.js
+++ webserver/client/js/controllers/DataCtrl.js
@@ -3,13 +3,13 @@ angular.module('Data', ['CommunicationService']).controller('DataCtrl', function
     $scope.update = function() {
         $scope.data += 1;
     }
-    $scope.message = "...";
+    $scope.orientation = 45;
     $scope.read_data = function() {
         communication.read_data(function(evt) {
             $scope.$apply(function() {
                 var hex = JSON.parse(evt.data);
                 console.log(hex);
-                $scope.message = parseInt(hex.count[2] + hex.count[3] + hex.count[0] + hex.count[1], 16);
+                $scope.orientation = parseInt(hex.count[2] + hex.count[3] + hex.count[0] + hex.count[1], 16);
             });
         });
     }
