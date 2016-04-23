6b97c4013e51fc2315f2b5b7eeecc8047d149dc3
Daniel Gorelick <dqgorelick@gmail.com>
2016-04-23 18:46:37 -0400

    working video processing, need to integrate with hardware

--- webserver/client/js/controllers/VideoCtrl.js
+++ webserver/client/js/controllers/VideoCtrl.js
@@ -11,13 +11,36 @@ angular.module('Video', ['CommunicationService']).controller('VideoCtrl', functi
     tracker.on('track', function(event) {
         $scope.rects = event.data;
         var count = 0;
+        var markers = [];
         $scope.rects.forEach(function(rect, num) {
             count++;
-            if (rect.height > rect.width * 2.5) {
+            if (rect.height > rect.width * 2 && rect.y > 200) {
                 console.log("RECT: " + num + " CENTER: (" + rect.x , rect.y + rect.height/2 + ") HEIGHT =", rect.height, "WIDTH =", rect.width);
+                markers.push(rect);
+                // console.log("RECT:", rect.x + rect.width/2);
             }
             if (num == $scope.rects.length - 1) {
-                console.log("NUMBER OF RECTANGLES:", count);
+                // console.log("NUMBER OF RECTANGLES:", count);
+                if (markers.length > 1) {
+                    if(markers[0].y + 20 > markers[1].y && markers[0].y - 20 < markers[1].y) {
+                        var breaker_center = (markers[0].x + markers[0].width/2 + markers[1].x + markers[1].width/2)/2;
+                        var breaker_status_color = (breaker_center < 322 && breaker_center > 318 ? "yellow" : "red");
+                        $scope.rects.push({
+                            "width": 2,
+                            "height": 480,
+                            "x": 319,
+                            "y": 0,
+                            "color":"green"
+                        });
+                        $scope.rects.push({
+                            "width": 4,
+                            "height": 480,
+                            "x": breaker_center - 2,
+                            "y": 0,
+                            "color": breaker_status_color
+                        });
+                    }
+                }
             }
         });
 
