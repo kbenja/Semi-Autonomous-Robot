553387fc660ba53942dae67151b973da1f5eb669
Daniel Gorelick <dqgorelick@gmail.com>
2016-04-24 15:37:45 +0000

    WIP video alignment c program

---
+++ eclipse/src/include/video_alignment.h
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <utility>
+#include "mraa.h"
+#include "navx_module.h"
+
+#ifndef VIDEO_ALIGNMENT_H
+#define VIDEO_ALIGNMENT_H
+
+class Video_Alignment {
+public:
+
+
+};
+#endif;
